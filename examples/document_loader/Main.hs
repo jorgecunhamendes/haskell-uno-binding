b031c048c034271b9f933aae8248244e31b7fba2
Jorge Mendes <jorgecunhamendes@gmail.com>
2015-08-15 23:07:31 +0100

    Removed temporary code and cleaned main function.

--- examples/document_loader/Main.hs
+++ examples/document_loader/Main.hs
@@ -7,52 +7,29 @@ module Main where
 
 import UNO
 
-import Control.Applicative ((<$>))
 import Foreign
 
-import Com.Sun.Star.Uno
-import Com.Sun.Star.Beans
-import Com.Sun.Star.Bridge
-import Com.Sun.Star.Frame
-import Com.Sun.Star.Uno.XInterface
 import Com.Sun.Star.Bridge.XUnoUrlResolver
 import Com.Sun.Star.Bridge.UnoUrlResolver
-import Com.Sun.Star.Beans.Property
 import Com.Sun.Star.Beans.XPropertySet
-import Com.Sun.Star.Beans.XPropertySetInfo
-import Com.Sun.Star.Uno.XComponentContext
-import Com.Sun.Star.Uno.DeploymentException
 import Com.Sun.Star.Frame.Desktop
-import Com.Sun.Star.Frame.XDesktop2
 import Com.Sun.Star.Frame.XComponentLoader
-import Com.Sun.Star.Lang.XMultiComponentFactory
 
-import Data.Text (Text, unpack, pack)
+import Data.Text (Text, pack)
 import System.Environment
 
 main :: IO ()
 main = do
-  oContext <- unoBootstrap
-  oUnoUrlResolver <- unoUrlResolverCreate oContext
-  xUnoUrlResolver <- queryXUnoUrlResolver oUnoUrlResolver
+  xContext <- mkReference . castPtr =<< unoBootstrap
+  oUnoUrlResolver <- unoUrlResolverCreate xContext
+  xUnoUrlResolver <- queryInterface oUnoUrlResolver
   xInterface <- resolve xUnoUrlResolver sConnectionString
-  fpType <- getUnoType (undefined :: XPropertySetRef)
-  xPropertySet <- withForeignPtr fpType $ \ pType -> do
-    aXPropertySet <- queryInterface xInterface (castPtr pType)
-    iface <- anyToInterface aXPropertySet False
-    return $ XPropertySetRef iface
+  xPropertySet <- queryInterface xInterface
   aXComponentContext <- getPropertyValue xPropertySet "DefaultContext"
-  xComponentContext <- XComponentContextRef <$> anyToInterface aXComponentContext True
+  xComponentContext <- fromAnyIO aXComponentContext
   --Reference < XDesktop2 > xComponentLoader = Desktop::create(xComponentContext);
-  withForeignPtr (unXComponentContextRef xComponentContext) $ \ oContext2 -> do
-    oDesktop <- Com.Sun.Star.Frame.Desktop.desktopCreate (castPtr oContext2)
-    -- xComponentLoader <- queryXComponentLoader oDesktop
-    xDesktop2 <- queryXDesktop2 oDesktop
-    fpType2 <- getUnoType (undefined :: XComponentLoaderRef)
-    xComponentLoader <- withForeignPtr fpType2 $ \ pType -> do
-      aXComponentLoader <- queryInterface (XInterfaceRef (unXDesktop2Ref xDesktop2)) (castPtr pType)
-      iface <- anyToInterface aXComponentLoader False
-      return $ XComponentLoaderRef iface
+  xDesktop2 <- Com.Sun.Star.Frame.Desktop.desktopCreate xComponentContext
+  xComponentLoader <- queryInterface xDesktop2
   -- Loads a component specified by an URL into the specified new or existing frame.
   -- OUString sAbsoluteDocUrl, sWorkingDir, sDocPathUrl, sArgDocUrl;
   --rtl_getAppCommandArg(0, &sArgDocUrl.pData);
@@ -60,12 +37,11 @@ main = do
   --osl::FileBase::getFileURLFromSystemPath( sArgDocUrl, sDocPathUrl);
   --osl::FileBase::getAbsoluteFileURL( sWorkingDir, sDocPathUrl, sAbsoluteDocUrl);
   --
-  --Reference< XComponent > xComponent = xComponentLoader->loadComponentFromURL( sAbsoluteDocUrl, OUString( "_blank" ), 0, Sequence < ::com::sun::star::beans::PropertyValue >() );
-    args <- getArgs
-    xComponent <- loadComponentFromURL xComponentLoader (pack $ args !! 0) "_blank" 0 nullPtr
+  args <- getArgs
+  xComponent <- loadComponentFromURL xComponentLoader (pack $ args !! 0) "_blank" 0 nullPtr
   -- dispose the local service manager
   --Reference< XComponent >::query( xMultiComponentFactoryClient )->dispose();
-    return ()
+  return ()
 
 sConnectionString :: Text
 sConnectionString = "uno:socket,host=localhost,port=2083;urp;StarOffice.ServiceManager"
--- examples/document_loader/Temp.hs
+++
@@ -1,9 +0,0 @@
-module Temp where
-
-import UNO
-import Foreign
-
-data Sequence a
-
-instance Service (ForeignPtr a) where
-  getInterface = castForeignPtr
